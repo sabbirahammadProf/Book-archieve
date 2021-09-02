926009fa38cae83d6396dd3ee2f03f9780dceaf8
sabbirahammadProf <sabbirahammad212@gmail.com>
2021-09-02 12:32:46 +0600

    all

--- js/main.js
+++ js/main.js
@@ -1,21 +1,54 @@
+// Loader 
+const loader = loaderVisibility => {
+    document.getElementById('loader').style.display = loaderVisibility;
+};
+// Blank alert
+const blankAlert = alertVisibility => {
+    document.getElementById('alert').style.display = alertVisibility;
+};
+// No result alert
+const noResult = noResultVisibility =>{
+    document.getElementById('no-result').style.display = noResultVisibility;
+};
+// Result
+const result = resultVisibility => {
+    document.getElementById('result').style.display = resultVisibility;
+};
 // Load books
 const loadBook = () => {
     const searchBar = document.getElementById('search-value');
     const searchText = searchBar.value;
-    const fetchUrl = `http://openlibrary.org/search.json?q=${searchText}`;
-    fetch(fetchUrl)
-    .then(response => response.json())
-    .then(data => showBooks(data));
+    result('none');
+    noResult('none');
+      if (searchBar.value !== '') {
+        // Load books by fetch
+        blankAlert('none');
+        loader('block');
+        const fetchUrl = `http://openlibrary.org/search.json?q=${searchText}`;
+        fetch(fetchUrl)
+        .then(response => response.json())
+        .then(data => showBooks(data));
+      } else {
+        // Blank alert show
+        blankAlert('block');
+        loader('none');
+      }
+    // Search input value after search complete
     searchBar.value = '';
 }
 
 // Show books
-const showBooks = (books) => {
+const showBooks = books => {
     const booksContainer = document.getElementById('books-container');
     const searchQueries = document.getElementById('search-queries');
     booksContainer.textContent = '';
-    searchQueries.textContent = '';
     const allBooks = books.docs;
+    // No result alert
+    if (books.num_found === 0) {
+        noResult('block');
+        loader('none');
+    } else {
+    // Show results
     searchQueries.innerHTML = `
     <p class="bg-primary text-white mx-2 py-2 px-4 rounded-pill">Total Result: ${books.num_found}</p>
     <p class="bg-primary text-white mx-2 py-2 px-4 rounded-pill">Search query: ${books.q}</p>
@@ -45,12 +78,21 @@ const showBooks = (books) => {
           } else {
               publisher = book.publisher;
           }
-  
+          
+          // Set cover image
+          let coverImage = book.cover_i;
+          if (coverImage === undefined) {
+              coverImage = 'https://images.reverb.com/image/upload/s--jbteVJS4--/t_card-square/v1561378170/fljqjjr3nik3juodhijc.jpg';
+          } else {
+              coverImage = `https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg`;
+          }
+
+          // Show books   
           const booksBody = document.createElement('div');
           booksBody.classList.add('col');
           booksBody.innerHTML = `
-            <div class="card">
-              <img src="https://covers.openlibrary.org/b/id/8706224-M.jpg" class="card-img-top border border-secondary" alt="..." width="100%" height="270px">
+            <div class="card shadow-lg">
+              <img src="${coverImage}" class="card-img-top border border-secondary" alt="..." width="100%" height="270px">
               <div class="card-body">
                 <h5 class="card-title">${book.title}</h5>
                 <p class="mt-3">Author name: ${authorName}</p>
@@ -59,6 +101,11 @@ const showBooks = (books) => {
               </div>
             </div>
           `;
+      // Append books
       booksContainer.appendChild(booksBody);
     });
-}
+    loader('none');
+    result('block');
+    noResult('none');
+    };
+};
